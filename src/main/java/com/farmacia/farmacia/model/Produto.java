113b1ef33da5b70463c2974bbcaaeddf43c80afb
vnalmenara <veronica_n_almenara@hotmail.com>
2021-01-26 18:18:47 -0300

    Task1-CRUD-Tabela-Produto

---
+++ src/main/java/com/farmacia/farmacia/model/Produto.java
@@ -0,0 +1,72 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table (name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private String tipo;
+	
+	@NotNull
+	private String indicacao;
+	
+	//Relação entre tabelas.
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getIndicacao() {
+		return indicacao;
+	}
+
+	public void setIndicacao(String indicacao) {
+		this.indicacao = indicacao;
+	}
+	
+}
